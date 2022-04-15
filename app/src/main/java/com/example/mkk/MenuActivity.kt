8633a2cfb499ff6d124a8eba8d8ed43bed743482
GrigoryKharchenko <96061286+GrigoryKharchenko@users.noreply.github.com>
2022-04-15 10:51:32 +0600

    [day3] change for pull requests

--- app/src/main/java/com/example/mkk/MenuActivity.kt
+++ app/src/main/java/com/example/mkk/MenuActivity.kt
@@ -1,36 +1,25 @@
 package com.example.mkk
 
-import android.content.DialogInterface
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.widget.ImageButton
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MenuActivity : AppCompatActivity() {
 
-    private var btnOpenShift: ImageButton? = null
+
+    private var btListReport: ConstraintLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-//        btnOpenShift = findViewById(R.id.btOpenShift)
+        btListReport = findViewById(R.id.btListReport)
 
-        openShiftClickListeners()
+        btListReport?.setOnClickListener{}
 
     }
 
-    private fun openShiftClickListeners(){
-
-        btnOpenShift?.setOnClickListener{openShiftClickDialog()}
-    }
-
-    private fun openShiftClickDialog(){
-        MaterialAlertDialogBuilder(this)
-            .setView(R.layout.edit_text_dialog)
-            .show()
-    }
 }
+
 
